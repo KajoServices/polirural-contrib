0b0d90ce43cae433595ef35f47580b60bb5dddf4
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2019-12-12 17:38:54 +0100

    Add update for clicking links, fix interaction and remove drag functionality

--- visualization/script.js
+++ visualization/script.js
@@ -1,48 +1,76 @@
 var svg = d3.select("svg"),
-width = +svg.attr("width"),
-height = +svg.attr("height");
+width   = +svg.attr("width"),
+height  = +svg.attr("height");
 
-d3.json('graph.json').then(function(graph) {
+//var graph_url = "graph.json"
+function graph_url(topic) {
+	var base_url = "http://178.62.250.117/api/v1/similarity_cluster/?";
+	var params = new URLSearchParams({
+		"username": "kajo_connect",
+		"api_key": "3d0a2921a8b57cccf4214753bd53eed5f2dcab0f",
+		"lang": "en",
+		"topic": topic,
+		"threshold": "0.6"
+	});
+
+	url = base_url + params.toString();
+	console.log(url);
+	return url;
+}
+
+var simulation, link, node;
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+d3.json(graph_url("cereal production")).then(function(graph) {
 	console.log(graph);
 	//var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-	var simulation = d3.forceSimulation()
-		.force("link", d3.forceLink().id(function(d) { return d.id; }))
-	 	.force("charge", d3.forceManyBody().strength(-500))
-		.force("center", d3.forceCenter(width / 2, height / 2));
-
-	var tooltipdiv = d3.select("body").append("div")
-		.attr("class", "tooltip")
-		.style("opacity", 0);
-
-	var link = svg.append("g")
+	simulation = d3.forceSimulation()
+    	.force("charge", d3.forceManyBody().strength(-1000))
+    	.force("link", d3.forceLink().id(function(d) { return d.id; }).distance(50))
+    	//.force("link", d3.forceLink().id(function(d) { return d.id; }))
+    	//.force("charge", d3.forceManyBody().strength(-500))
+		.force("center", d3.forceCenter(width / 2, height / 2))
+    	.force("x", d3.forceX())
+    	.force("y", d3.forceY());
+    	//.alphaTarget(1)
+    	//.on("tick", ticked);
+	
+	link = svg.append("g")
 		.attr("class", "links")
-		.selectAll("line")
-		.data(graph.links)
-		.enter().append("line")
-		.attr("stroke-width", function(d) { return Math.sqrt(1+d.value); });
-
-	var node = svg.append("g")
+		.attr("stroke", "#000")
+		.attr("stroke-width", 1.5)
+		.selectAll(".link");
+	
+	node = svg.append("g")
 		.attr("class", "nodes")
-		.selectAll("g")
-		.data(graph.nodes)
-		.enter().append("g")
-	var circles = node.append("circle")
+		.attr("stroke", "#fff")
+		.attr("stroke-width", 1.5)
+		.selectAll(".node");
+
+	restart(graph);
+});
+
+function restart(graph) {
+	// Apply the general update pattern to the nodes.
+	node = node.data(graph.nodes, function(d) { return d.id;});
+	node.exit().remove();
+
+	var g = node.enter().append("g")
+		.attr("class", "node");
+
+	g.append("circle")
 		.attr("r", function(d) { return Math.sqrt(d.size)*3;})
 		.attr("fill", function(d) { return d.color; })
 		.attr("vector-effect", "non-scaling-stroke")
 		.attr("stroke", "white")
 		.attr("stroke-width", "2")
-		.on("click", function(d) { return klick(d.id)})
-		.call(d3.drag()
-		.on("start", dragstarted)
-		.on("drag", dragged)
-		.on("end", dragended));
-
-	var lables = node.append("text")
+		.on("click", function(d) { return click(d.id)});
+	
+	g.append("text")
 		.attr("text-anchor", "middle")
 		.attr("font-weight", "bold")
-		.style("font", "18px calibri")
+		.style("font", "12px calibri")
 		.attr("fill", "#2D0004")
 		.attr("vector-effect", "non-scaling-stroke")
 		.attr("stroke", "brown")
@@ -52,50 +80,36 @@ d3.json('graph.json').then(function(graph) {
 		})
 		.attr('x', 0)
 		.attr('y', -10)
-		.on("click", function(d) { return klick(d.id)});
+		.on("click", function(d) { return click(d.id)});
 
-	node.append("title")
-		.text(function(d) { return d.id; });
+	node = g.merge(node);
 
-	simulation
-		.nodes(graph.nodes)
-		.on("tick", ticked);
+	// Apply the general update pattern to the links.
+	link = link.data(graph.links, function(d) { return d.source.id + "-" + d.target.id; });
+	link.exit().remove();
+	link = link.enter().append("line").merge(link);
 
-	simulation.force("link").distance(100).strength(3)
-		.links(graph.links);
+	// Update and restart the simulation.
+	simulation.nodes(graph.nodes);
+	simulation.force("link").links(graph.links);
+	simulation.on("tick", ticked);
+	simulation.alpha(1).restart();
+}
 
-	function ticked() {
-		link
-		.attr("x1", function(d) { return d.source.x; })
-		.attr("y1", function(d) { return d.source.y; })
-		.attr("x2", function(d) { return d.target.x; })
-		.attr("y2", function(d) { return d.target.y; });
+function ticked() {
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 
-		node
-		.attr("transform", function(d) {
-			return "translate(" + d.x + "," + d.y + ")";
-		})
-	}
+  node.attr("transform", function(d) { 
+  	return "translate(" + d.x + "," + d.y + ")"; });
+}
 
-	function dragstarted(d) {
-		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-		d.fx = d.x;
-		d.fy = d.y;
-	}
-
-	function dragged(d) {
-		d.fx = d3.event.x;
-		d.fy = d3.event.y;
-	}
-
-	function dragended(d) {
-		if (!d3.event.active) simulation.alphaTarget(0);
-		d.fx = null;
-		d.fy = null;
-	}
-
-	function klick(a){
-		console.log("klick : "+a);
-		//window.location.assign("/txtmining/similar/"+a);
-	}
-});
+function click(topic){
+	console.log("click : " + topic);
+	d3.json(graph_url(topic)).then(function(graph) {
+		console.log(graph);
+		restart(graph);
+	});
+}
